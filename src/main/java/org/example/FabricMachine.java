28375b6033645432f86168b7f10de1f53b269dfb
илья <126683197+varfolomeevilya@users.noreply.github.com>
2023-10-05 10:29:03 +0300

    Update FabricMachine.java

--- src/main/java/org/example/FabricMachine.java
+++ src/main/java/org/example/FabricMachine.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class FabricMachine {
+    protected String name;
+    protected String model;
+    protected double grandeCoffeeContainer;
+    protected int grandeCoffeContainer;
+    protected int usedCoffeeTank;
+  
+   public FabricMachine(String name, String model, double grandeCoffeeContainer, int grandeCoffeContainer, int usedCoffeeTank){
+     
+   }
 }
